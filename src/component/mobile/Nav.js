73e4a0bb3be496acd386de4ff8f6d14d62a03cc1
Johnnie Hicks <johnnie@Johnnies-MacBook-Pro.local>
2021-09-03 20:52:12 -0400

    intial commit

---
+++ src/component/mobile/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Nav extends React.Component{
+            
+    render(){
+        return(
+            <div className="mobile-nav-main">
+            </div>
+        )
+    }
+}
+
+export default Nav;
